6fe11fc25c0550545d7e57035dd1793ebb2c6511
kmbarnett <kmbarnett94@gmail.com>
2020-03-24 16:26:58 -0600

    Add structure

--- src/NeighborhoodsPage/NeighborhoodsPage.js
+++ src/NeighborhoodsPage/NeighborhoodsPage.js
@@ -1,3 +1,26 @@
-// class component 
+// class component
 
 // state has a neighborhoods property which is where we store the fetched neighborhood data
+import React from 'react';
+import './NeighborhoodsPage.css';
+import Nav from './../Nav/Nav.js'
+
+class NeighborhoodsPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <Nav />
+        <h1>Neighborhoods Page</h1>
+      </main>
+    )
+  }
+}
+
+export default NeighborhoodsPage
