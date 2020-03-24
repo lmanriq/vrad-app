8e8f890ad7c3235a92b7ca88fd0d016c86ba11b3
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-24 17:40:15 -0600

    Delete unused import and comment

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './LandingPage.css';
 import LoginForm from '../LoginForm/LoginForm.js'
-import { Link } from 'react-router-dom'
 
 function LandingPage(props) {
-  // form component will need props.updateUser
   return (
     <div>
       <h1>Welcome to VRAD</h1>
-      <LoginForm updateUser = {props.updateUser}/>
+      <LoginForm 
+        updateUser = {props.updateUser}/>
     </div>
   )
 }
