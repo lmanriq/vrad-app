190a4ab20946f0c510dc1a9bcbab5296c78c5a80
kmbarnett <kmbarnett94@gmail.com>
2020-03-24 16:48:04 -0600

    Add some starting notes

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './LandingPage.css';
 import { Link } from 'react-router-dom'
 
-function LandingPage() {
+function LandingPage(props) {
+  // form component will need props.updateUser
   return (
     <div>
       <h1>Landing Page</h1>
