82b7c9e1ba82391c6dad61c8ce347a1bc4ae9200
kmbarnett <kmbarnett94@gmail.com>
2020-03-24 16:26:02 -0600

    Add set up

--- src/ListingsPage/ListingsPage.js
+++ src/ListingsPage/ListingsPage.js
@@ -1,5 +1,28 @@
 // class component
 
-// favoriteListings array in state that stores fav ids 
+// favoriteListings array in state that stores fav ids
 // isFavorite prop passed down to card (check if included in the favs array)
 // addToFavorites and removeFromFavorites methods to add/subtract from the array
+import React from 'react';
+import './ListingsPage.css';
+import Nav from './../Nav/Nav.js'
+
+class ListingsPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <Nav />
+        <h1>Listings Page</h1>
+      </main>
+    )
+  }
+}
+
+export default ListingsPage
