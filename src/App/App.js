45ef282457e3e6c93043c3dd0029e21479ac6d49
kmbarnett <kmbarnett94@gmail.com>
2020-03-24 16:23:44 -0600

    Add setup router to work"

--- src/App/App.js
+++ src/App/App.js
@@ -1,58 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import LandingPage from './../LandingPage/LandingPage.js'
+import NeighborhoodsPage from './../NeighborhoodsPage/NeighborhoodsPage.js'
+import ListingsPage from './../ListingsPage/ListingsPage.js'
+
 
 // class component
 
-function App() {
+class App extends React.Component {
   //When a user clicks on the View Listings, the user should then be redirected to /areas/:area_id/listings/.
   //NOTE: Be careful on how to structure this route. The parts of the route that are dynamic (ex: /:area_id) should not actually include a colon - it is simply to show that this part of the route should change. An example of what a completed route would look like could be: /areas/66
-  // http://localhost:3001/api/v1/areas	
-  // http://localhost:3001/api/v1/areas/:id	
-  // http://localhost:3001/api/v1/listings/:id	
+  // http://localhost:3001/api/v1/areas
+  // http://localhost:3001/api/v1/areas/:id
+  // http://localhost:3001/api/v1/listings/:id
 
   // const BASE = 'http://localhost:3001/api/v1/'
   // const AREAS = 'areas'
 
   // fetch(BASE + AREAS + AREA_ID)
+  constructor() {
+    super()
+    this.state = {
+    }
+  }
 
-  return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Switch> 
-          <Router path="/Neighborhoods" component={NeighborhoodsPage}/>
-          <Router path="/Listings" component={ListingsPage}/>
-          <Router path="/" exact component={LandingPage}/>
-        </Switch>
-      </div>
-    </Router>
-  );
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/neighborhoods" component={NeighborhoodsPage}/>
+            <Route path="/listings/:id" component={ListingsPage}/>
+            <Route path="/listings" exact component={ListingsPage}/>
+            <Route path="/" exact component={LandingPage}/>
+          </Switch>
+        </div>
+      </Router>
+    )};
 }
 
 export default App;
 
 
 
-render () {
-  return (
-    <Header />
-    <Nav />
-    <main>
-      {neighborhoodCards}
-    </main>
-  )
-}
-
-
-const Listings = () => {}
-render () {
-  return (
-    <Header />
-    <Nav />
-    <main>
-      {listingCards}
-    </main>
-  )
-}
+// render () {
+//   return (
+//     <Header />
+//     <Nav />
+//     <main>
+//       {neighborhoodCards}
+//     </main>
+//   )
+// }
+//
+//
+// const Listings = () => {}
+// render () {
+//   return (
+//     <Header />
+//     <Nav />
+//     <main>
+//       {listingCards}
+//     </main>
+//   )
+// }
