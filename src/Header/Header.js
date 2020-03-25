e9c371a900084997e5e9a20e32e85514b11be73b
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-25 08:59:46 -0600

    Add basic component and stylings

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,1 +1,26 @@
-// functional
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom'
+
+const Header = (props) => {
+  return (
+    <header>
+      <img className="small-logo" src="/logo.svg" alt="blucifinder logo"/>
+      <section className="user-info">
+        <section className="welcome-details">
+          <h1>Welcome, {props.currentUser.name}</h1>
+          <p>We're here to help with all of your {props.currentUser.purpose} needs</p>
+        </section>
+        <section className="avatar-section"> 
+          <img className="avatar-photo" src="/avatar-photo.png" alt="avatar" />
+          <Link to='/'>
+            <button type="button">Log Out</button>
+          </Link>
+        </section>
+      </section>
+    </header>
+  )
+}
+
+export default Header;
+
