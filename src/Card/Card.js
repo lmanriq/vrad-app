64f3195cc60e04a35567b9e24b83e8e252bca917
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-25 16:44:06 -0600

    Rename card directory

--- src/Card/Card.js
+++
@@ -1,5 +0,0 @@
-// functional component
-
-// NeighborhoodsCard - Name, picture, view listings btn 
-// Listings card - Name, picture, view details btn, favorite icon
-// card render: const checked = isFavorite, render checkbox as checked
