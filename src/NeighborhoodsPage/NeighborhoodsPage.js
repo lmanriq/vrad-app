ed596d56d90b88b1d3284906bc34dc3d4dccf4d9
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-25 16:43:38 -0600

    Pass props to area cards and add them to the container

--- src/NeighborhoodsPage/NeighborhoodsPage.js
+++ src/NeighborhoodsPage/NeighborhoodsPage.js
@@ -5,6 +5,8 @@ import React from 'react';
 import './NeighborhoodsPage.css';
 import Nav from './../Nav/Nav.js'
 import Header from './../Header/Header.js'
+import Card from '../NeighborhoodCard/NeighborhoodCard.js'
+
 
 class NeighborhoodsPage extends React.Component {
   constructor(props) {
@@ -15,12 +17,24 @@ class NeighborhoodsPage extends React.Component {
   }
 
   render() {
+    console.log(this.props.areas)
+    const areaCards = this.props.areas.map(area => {
+      return <Card 
+        areaNickname = {area.areaNickname}
+        name = {area.name}
+        id = {area.id}
+        key = {area.id}
+        location = {area.location}
+        description = {area.about}
+      />
+    })
+    
     return (
       <section className="main-page">
         <Header currentUser = {this.props.currentUser}/>
         <Nav />
         <section className="container">
-          <p>neighborhoods</p>
+          {areaCards}
         </section>
       </section>
     )
