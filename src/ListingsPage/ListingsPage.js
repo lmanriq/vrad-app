854ce116a8adff5be30076a8bfce1065c0ba90c8
kmbarnett <kmbarnett94@gmail.com>
2020-03-25 15:50:04 -0600

    Add listings Fetch

--- src/ListingsPage/ListingsPage.js
+++ src/ListingsPage/ListingsPage.js
@@ -6,17 +6,27 @@
 import React from 'react';
 import './ListingsPage.css';
 import Nav from './../Nav/Nav.js'
+import ListingsCard from './../ListingsCard/ListingsCard.js'
 import Header from './../Header/Header.js'
 
 class ListingsPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      listings: [],
     }
   }
 
+  componentDidMount() {
+    const { listings } = this.state
+    fetch('http://localhost:3001/api/v1/listings')
+      .then(res => res.json())
+      .then(data => this.setState({listings: [...listings, ...data.listings]}))
+      .catch(err => console.log(err.message))
+  }
+
   render() {
+    const { listings } = this.state
     return (
       <section className="main-page">
         <Header currentUser = {this.props.currentUser}/>
