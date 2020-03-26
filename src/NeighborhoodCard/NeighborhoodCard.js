e8d5922fc79cadb39a1e3dd7f807b11175c69c7d
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-26 14:47:08 -0600

    Destructure props and add link to view listings btn

--- src/NeighborhoodCard/NeighborhoodCard.js
+++ src/NeighborhoodCard/NeighborhoodCard.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './NeighborhoodCard.css';
+import { Link } from 'react-router-dom'
 
 // NeighborhoodsCard - Name, picture, view listings btn 
 // Listings card - Name, picture, view details btn, favorite icon
 // card render: const checked = isFavorite, render checkbox as checked
 
-const NeighborhoodCard = (props) => {
-  console.log(props.listings)
+const NeighborhoodCard = ({ id, areaNickname, name, location, description }) => {
+  // render a ListingsPage component, pass listings as props
+  // onClick={render ListingsPage () => getListingsData}
+  
+
   return (
-    <article id={props.id} className="card neighborhood-card">
-      <h4>{props.areaNickname}</h4>
-      <p>({props.name})</p>
-      <h5>{props.location}</h5>
-      <p>{props.description}</p>
-      <button>View Listings</button>
+    <article id={id} className="card neighborhood-card">
+      <h4>{areaNickname}</h4>
+      <p>({name})</p>
+      <h5>{location}</h5>
+      <p>{description}</p>
+      <Link to={`neighborhoods/${id}/listings`}> 
+        <button type="button">View Listings</button>
+      </Link>
     </article>
   )
 }
