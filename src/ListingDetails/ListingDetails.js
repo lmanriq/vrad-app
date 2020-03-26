d293bcc3f680a5ddca59a7739ee2e22b98518c07
kmbarnett <kmbarnett94@gmail.com>
2020-03-26 16:39:33 -0600

    Add structure to listing details

--- src/ListingDetails/ListingDetails.js
+++ src/ListingDetails/ListingDetails.js
@@ -1,2 +1,37 @@
 // functional component
 
+import React from 'react';
+import './ListingDetails.css';
+import Nav from './../Nav/Nav.js'
+import Header from './../Header/Header.js'
+
+class ListingDetails extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      listing: null
+    }
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:3001/api/v1/listings/${this.props.id}`)
+      .then(res => res.json())
+      .then(data => this.setState({listing: {...data}}))
+      .catch(err => console.log(err.message))
+  }
+
+  render(){
+    return (
+      <section className="main-page">
+        <Header currentUser = {this.props.currentUser}/>
+        <Nav />
+        <section className="container listing-container">
+          <h1>Listing</h1>
+        </section>
+      </section>
+    )
+
+  }
+}
+
+export default ListingDetails
