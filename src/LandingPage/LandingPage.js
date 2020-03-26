23a0aba9fe180c8da06c25f4245e4bd93f93e660
kmbarnett <kmbarnett94@gmail.com>
2020-03-26 15:31:06 -0600

    Add some style stuff

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ import LoginForm from '../LoginForm/LoginForm.js'
 import { ReactComponent as Logo }  from '../logo.svg'
 
 
-function LandingPage(props) {
+function LandingPage({ updateUser }) {
   return (
     <section className='landing-page'>
       <div className='container-landing'>
@@ -13,7 +13,7 @@ function LandingPage(props) {
         </div>
         <h1>Welcome to Blucifinder</h1>
         <LoginForm
-          updateUser = {props.updateUser}/>
+          updateUser = { updateUser }/>
       </div>
     </section>
   )
