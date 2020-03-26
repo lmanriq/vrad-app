6d4ace72d1789e372956929054fabeef01e9b627
kmbarnett <kmbarnett94@gmail.com>
2020-03-26 11:23:55 -0600

    Add horse shadow to landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './LandingPage.css';
 import LoginForm from '../LoginForm/LoginForm.js'
-import logo from '../logo.svg'
+import { ReactComponent as Logo }  from '../logo.svg'
 
 
 function LandingPage(props) {
   return (
     <section className='landing-page'>
       <div className='container-landing'>
-        <img className='icon-landing' alt='Blucifer' src={logo}/>
+        <div className='logo-box'>
+          <Logo />
+        </div>
         <h1>Welcome to Blucifinder</h1>
         <LoginForm
           updateUser = {props.updateUser}/>
