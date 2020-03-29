61aa253c10276856e7ef20c99aebb45c7acbfe43
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-29 09:57:30 -0600

    Add and style listing images

--- src/ListingDetails/ListingDetails.js
+++ src/ListingDetails/ListingDetails.js
@@ -10,6 +10,7 @@ class ListingDetails extends React.Component {
     super(props)
     this.state = {
       listing: {
+        listing_id: null, 
         name: null,
         address: {
           street: null,
@@ -35,8 +36,8 @@ class ListingDetails extends React.Component {
   }
 
   render(){
-    // console.log(this.state.listing)
-    const { name, address, details, area } = this.state.listing;
+    console.log(this.state.listing)
+    const { listing_id, name, address, details, area } = this.state.listing;
 
     return (
       <section className="main-page">
@@ -48,10 +49,15 @@ class ListingDetails extends React.Component {
             <p>{address.street} Denver, CO {address.zip}</p>
           </section>
           {details.superhost && <p className="superhost"><img src="/images/thunder.svg" alt="lightning logo" />Superhost<img src="/images/thunder.svg" alt="lightning logo" /></p>}
-          <p>Cost Per Night: {details.cost_per_night}</p>
+          <p>Cost Per Night: ${details.cost_per_night}</p>
           <p>Beds: {details.beds}</p>
           <p>Baths: {details.baths}</p>
           <p>Features: {details.features.join(', ')}</p>
+          <section className="listing-imgs">
+            <img src={`/images/${listing_id}_a.jpg`} alt={`${name} first`}/>
+            <img src={`/images/${listing_id}_b.jpg`} alt={`${name} second`}/>
+            <img src={`/images/${listing_id}_c.jpg`} alt={`${name} third`}/>
+          </section>
         </section>
       </section>
     )
