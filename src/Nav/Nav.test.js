007b29ab482ea0a8bd394dd1f84ce8f4319bfd9a
kmbarnett <kmbarnett94@gmail.com>
2020-03-30 16:00:30 -0600

    Add path testing

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Nav from './Nav';
-import { MemoryRouter as Router, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, NavLink } from 'react-router-dom'
 
 function renderNav() {
   const utils = render(
   <Router>
-    <Nav />
+    <Nav favoritesLength={0}/>
   </Router>
   )
   return utils
@@ -19,19 +19,22 @@ describe('Nav', () => {
     const header = getByText('Blucifinder')
     const neighborhoodsBtn = getByText('Neighborhoods')
     const listingsBtn = getByText('Listings')
+    const favoriteBtn = getByText('Favorites')
     expect(header).toBeInTheDocument()
     expect(neighborhoodsBtn).toBeInTheDocument()
     expect(listingsBtn).toBeInTheDocument()
+    expect(favoriteBtn).toBeInTheDocument()
   })
 
-  it('Should Have its exist', () => {
-    const { getByText } = renderNav()
-    const header = getByText('Blucifinder')
+  it('Should navigate', () => {
+    const { getByText, debug } = renderNav()
     const neighborhoodsBtn = getByText('Neighborhoods')
     const listingsBtn = getByText('Listings')
-    expect(header).toBeInTheDocument()
-    expect(neighborhoodsBtn).toBeInTheDocument()
-    expect(listingsBtn).toBeInTheDocument()
+    debug()
+    // fireEvent.click(listingsBtn)
+    fireEvent.click(neighborhoodsBtn)
+    // expect(location.pathname).toBe('/listings')
+    expect(location.pathname).toBe('/neighborhoods')
   })
 
 
