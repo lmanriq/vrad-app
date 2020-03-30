a0c873f92ced0573ebc2e22f245c41b9ba5f7f2b
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-30 17:53:11 -0600

    Add test for handling favorites click

--- src/ListingDetails/ListingDetails.test.js
+++ src/ListingDetails/ListingDetails.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitForElement } from '@testing-library/react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
 import ListingDetails from './ListingDetails';
 import '@testing-library/jest-dom'
 import { BrowserRouter } from "react-router-dom";
@@ -10,26 +10,57 @@ describe ('Listing Details', () => {
       name: 'Bob',
       purpose: 'business'
     }
+    const mockCheckIsFavorite = jest.fn();
+    const mockHandleFavorites = jest.fn();
     const { getByText, getByTestId } = render (
       <BrowserRouter>
         <ListingDetails
           currentUser = {currentUser}
+          favoritesLength = {5}
+          handleFavorites = {mockCheckIsFavorite}
+          checkIsFavorite = {mockHandleFavorites}
           id = {3}
         />
       </BrowserRouter>
     )
     expect(getByTestId('details-section')).toBeInTheDocument();
-    const sampleCard = await waitForElement(() => getByText('Hip RiNo Party Spot'));
-    const superhost = await waitForElement(() => getByText('Superhost'));
-    const cost = await waitForElement(() => getByText('Cost Per Night: $420'));
-    const beds = await waitForElement(() => getByText('Beds: 3'));
-    const baths = await waitForElement(() => getByText('Baths: 2.5'));
-    const features = await waitForElement(() => getByText('Features: hot tub, espresso machine'));
-    expect(sampleCard).toBeInTheDocument();
+    const sampleDetails = await waitFor(() => getByText('Hip RiNo Party Spot'));
+    const superhost = await waitFor(() => getByText('Superhost'));
+    const cost = await waitFor(() => getByText('Cost Per Night: $420'));
+    const beds = await waitFor(() => getByText('Beds: 3'));
+    const baths = await waitFor(() => getByText('Baths: 2.5'));
+    const features = await waitFor(() => getByText('Features: hot tub, espresso machine'));
+    expect(sampleDetails).toBeInTheDocument();
     expect(superhost).toBeInTheDocument();
     expect(cost).toBeInTheDocument();
     expect(beds).toBeInTheDocument();
     expect(baths).toBeInTheDocument();
     expect(features).toBeInTheDocument();
+  })
+
+  it('can be favorites', async () => {
+    const currentUser = {
+      name: 'Bob',
+      purpose: 'business'
+    }
+    const mockCheckIsFavorite = jest.fn();
+    const mockHandleFavorites = jest.fn();
+    const { getByTestId, getByText } = render (
+      <BrowserRouter>
+        <ListingDetails
+          currentUser = {currentUser}
+          favoritesLength = {5}
+          handleFavorites = {mockHandleFavorites}
+          checkIsFavorite = {mockCheckIsFavorite}
+          id = {3}
+        />
+      </BrowserRouter>
+    )
+    const sampleDetails = await waitFor(() => getByText('Hip RiNo Party Spot'));
+    expect(sampleDetails).toBeInTheDocument();
+    const checkbox = getByTestId('favorites-checkbox');
+    expect(checkbox).toBeInTheDocument();
+    fireEvent.click(checkbox);
+    expect(mockHandleFavorites).toHaveBeenCalledWith(3);
   })
 })
