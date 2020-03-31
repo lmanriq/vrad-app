442bfe9de2d80f7c5e4e64657cd0b75a4f62fa11
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-30 18:53:56 -0600

    Add nav proptypes

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Nav.css';
 import { NavLink } from 'react-router-dom'
 
@@ -27,3 +28,7 @@ const Nav = ({ favoritesLength }) => {
 }
 
 export default Nav;
+
+Nav.propTypes = {
+  favoritesLength: PropTypes.number
+}
