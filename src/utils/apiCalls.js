8c28f72512509b787a9b2351f857e5998de52d9e
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-31 15:03:17 -0600

    Add remaining broken out fetch calls

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -15,3 +15,25 @@ export const fetchNeighborhoodData = (signal) => {
       return Promise.all(promises)
     })
 }
+
+export const fetchAllListingsData = (signal) => {
+  return fetch('http://localhost:3001/api/v1/listings', { signal })
+      .then(res => res.json())
+}
+
+export const fetchAreaListingsData = (id, signal) => {
+  return fetch(`http://localhost:3001/api/v1/areas/${id}`, { signal })
+      .then(res => res.json())
+      .then(data => data.listings)
+      .then(listings => {const promises = listings.map(listing => {
+        return fetch('http://localhost:3001' + listing)
+                .then(res => res.json())
+          })
+          return Promise.all(promises)
+        })
+}
+
+export const fetchListingDetailsData = (id, signal) => {
+  return fetch(`http://localhost:3001/api/v1/listings/${id}`, { signal })
+      .then(res => res.json())
+}
