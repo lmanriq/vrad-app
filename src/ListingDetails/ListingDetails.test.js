58c63790be2c5420bd68159c21ff3caef34472e6
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-31 16:16:33 -0600

    Add listings details fetch test

--- src/ListingDetails/ListingDetails.test.js
+++ src/ListingDetails/ListingDetails.test.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import { render, waitFor, fireEvent } from '@testing-library/react';
 import ListingDetails from './ListingDetails';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 import { BrowserRouter } from "react-router-dom";
+import { fetchListingDetailsData } from '../utils/apiCalls';
+jest.mock('../utils/apiCalls')
 
 describe ('Listing Details', () => {
   it('renders text that we expect', async () => {
+    fetchListingDetailsData.mockResolvedValueOnce({
+      "listing_id": 3,
+      "area_id": 590,
+      "name": "Hip RiNo Party Spot",
+      "address": {
+      "street": "2250 Lawrence St",
+      "zip": "80205"
+      },
+      "details": {
+      "neighborhood_id": 5124122,
+      "superhost": true,
+      "seller_source": "91jss1",
+      "beds": 3,
+      "baths": 2.5,
+      "cost_per_night": 420,
+      "features": [
+      "hot tub",
+      "espresso machine"
+      ]
+      },
+      "dev_id": "u4gh2j",
+      "area": "rino",
+      "db_connect": 834470
+    })
     const currentUser = {
       name: 'Bob',
       purpose: 'business'
@@ -40,6 +66,30 @@ describe ('Listing Details', () => {
   })
 
   it('can be favorited', async () => {
+    fetchListingDetailsData.mockResolvedValueOnce({
+      "listing_id": 3,
+      "area_id": 590,
+      "name": "Hip RiNo Party Spot",
+      "address": {
+      "street": "2250 Lawrence St",
+      "zip": "80205"
+      },
+      "details": {
+      "neighborhood_id": 5124122,
+      "superhost": true,
+      "seller_source": "91jss1",
+      "beds": 3,
+      "baths": 2.5,
+      "cost_per_night": 420,
+      "features": [
+      "hot tub",
+      "espresso machine"
+      ]
+      },
+      "dev_id": "u4gh2j",
+      "area": "rino",
+      "db_connect": 834470
+    })
     const currentUser = {
       name: 'Bob',
       purpose: 'business'
