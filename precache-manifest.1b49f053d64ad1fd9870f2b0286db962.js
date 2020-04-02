29c90b1e9372c3d7bbfd36b97cf843d14f367e33
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-02 17:38:59 -0600

    Updates

---
+++ precache-manifest.1b49f053d64ad1fd9870f2b0286db962.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9e522742cc54c0cc09016f3c6cd77e",
+    "url": "/vrad-app/index.html"
+  },
+  {
+    "revision": "443121e43bb0c826f1cd",
+    "url": "/vrad-app/static/css/main.44561d65.chunk.css"
+  },
+  {
+    "revision": "6a070803e947e0bf313b",
+    "url": "/vrad-app/static/js/2.751c36ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vrad-app/static/js/2.751c36ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443121e43bb0c826f1cd",
+    "url": "/vrad-app/static/js/main.c4cbc0ed.chunk.js"
+  },
+  {
+    "revision": "b55331e1616f6f4be9c5",
+    "url": "/vrad-app/static/js/runtime-main.d515b884.js"
+  },
+  {
+    "revision": "cdacb1ab9bfe4e7f793714add56171e7",
+    "url": "/vrad-app/static/media/004-checked.cdacb1ab.svg"
+  },
+  {
+    "revision": "d242ba2e6dbe8da23e87ab969e0197b3",
+    "url": "/vrad-app/static/media/005-unchecked.d242ba2e.svg"
+  },
+  {
+    "revision": "626ab9a7e8b7e948916be84ab4f86dab",
+    "url": "/vrad-app/static/media/logo.626ab9a7.svg"
+  }
+]);
